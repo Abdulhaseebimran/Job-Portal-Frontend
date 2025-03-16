b34dd051ae4c5f7b1ba1554c27045f3b27a39836
Abdulhaseebimran <haseebimran0001@gmail.com>
2025-03-16 20:03:34 +0500

    haseeb update

--- frontend/utils/constant.js
+++ frontend/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT = "https://job-portal-backend.vercel.app/user"
-export const JOB_API_END_POINT = "https://job-portal-backend.vercel.app/job"
-export const Application_API_END_POINT = "https://job-portal-backend.vercel.app/application"
-export const Company_API_END_POINT = "https://job-portal-backend.vercel.app/company"
+export const USER_API_END_POINT = "https://job-portal-backend-khaki-eight.vercel.app/user"
+export const JOB_API_END_POINT = "https://job-portal-backend-khaki-eight.vercel.app/job"
+export const Application_API_END_POINT = "https://job-portal-backend-khaki-eight.vercel.app/application"
+export const Company_API_END_POINT = "https://job-portal-backend-khaki-eight.vercel.app/company"
